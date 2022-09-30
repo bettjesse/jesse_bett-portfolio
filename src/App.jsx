1eaac799f9ad69f458c1e1b652cf466deb897e70
jesse <jessebett1999@gmail.com>
2022-09-30 16:32:12 +0300

    portfolio website

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import style from './style'
+
+
+const App = () => {
+  return (
+
+    <div className=" w-full overflow-hidden ">
+      
+<p className='text-[20px]'>hshshshs</p>
+         
+        </div>
+
+   
+      
+ 
+  )
+}
+  
+
+export default App
